800663037637972e319b8721e36122bef312e242
AkshitRaizada <105885452+AkshitRaizada@users.noreply.github.com>
2022-08-12 21:34:03 +0530

    Add files via upload

---
+++ jackal_velodyne/worlds/aws-robomaker-hospital-world-ros1/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+python3 -m pip install -r requirements.txt
+python3 fuel_utility.py download \
+-m XRayMachine -m IVStand -m BloodPressureMonitor -m BPCart -m BMWCart \
+-m CGMClassic -m StorageRack -m Chair \
+-m InstrumentCart1 -m Scrubs -m PatientWheelChair \
+-m WhiteChipChair -m TrolleyBed -m SurgicalTrolley \
+-m PotatoChipChair -m VisitorKidSit -m FemaleVisitorSit \
+-m AdjTable -m MopCart3 -m MaleVisitorSit -m Drawer \
+-m OfficeChairBlack -m ElderLadyPatient -m ElderMalePatient \
+-m InstrumentCart2 -m MetalCabinet -m BedTable -m BedsideTable \
+-m AnesthesiaMachine -m TrolleyBedPatient -m Shower \
+-m SurgicalTrolleyMed -m StorageRackCovered -m KitchenSink \
+-m Toilet -m VendingMachine -m ParkingTrolleyMin -m PatientFSit \
+-m MaleVisitorOnPhone -m FemaleVisitor -m MalePatientBed \
+-m StorageRackCoverOpen -m ParkingTrolleyMax \
+-d fuel_models --verbose
